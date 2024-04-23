71fcf99970f3bfa38cbe2632795d906c28988a2d
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-23 10:13:58 +0200

    trying to add a motion framer anime

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./components/Nav.jsx";
 import CardComp from "./components/Card.jsx";
 import { Snippet } from "@nextui-org/react";
 import { Image } from "@nextui-org/react";
+import { motion } from "framer-motion";
 
 export default function App() {
   return (
@@ -14,9 +15,15 @@ export default function App() {
       <div className="flex flex-row items-center justify-center p-4 space-x-4">
         <CardComp />
       </div>
-      <div className="flex flex-row items-center justify-center">
-        <Image width={400} height={300} alt="NextUI hero Image with delay" isZoomed src="https://app.requestly.io/delay/300/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg" />
-      </div>
+      <motion.div
+        initial={{ y: -10 }} // Initial animation state
+        animate={{ y: [10, -10], transition: { yoyo: Infinity, duration: 3, ease: "easeInOut" } }} // Animation properties
+        className="mx-2"
+      >
+        <div className="flex flex-row items-center justify-center">
+          <Image width={400} height={300} alt="NextUI hero Image with delay" isZoomed src="https://app.requestly.io/delay/300/https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg" />
+        </div>
+      </motion.div>
       <div className="flex flex-wrap items-center justify-center gap-4 p-4">
         <Snippet color="default">
           npm install $ShitCoinProject
